95504aeb78e28f179ec4764be8bb1f27df4c2282
Lucas Mariano do carmo <lucas.mariano.carmo@live.com>
2022-11-20 19:28:39 -0300

    .

--- DDD.Shared.Core/Shared/Contracts/IDomainEventPublisher.cs
+++ DDD.Shared.Core/Shared/Contracts/IDomainEventPublisher.cs
@@ -1,11 +1,10 @@
-﻿using DDD.Shared.Core.Shared.Contracts;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DDD.Shared.Core.Shared.Events
+namespace DDD.Shared.Core.Shared.Contracts
 {
     public interface IDomainEventPublisher<TEvent> where TEvent : IEvent
     {
